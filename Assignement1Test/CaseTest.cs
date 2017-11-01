91ced12b13d3be31ae5638e8230837e46b763d83
pgreksic <petar.greksic@fer.hr>
2017-11-01 19:25:00 +0100

    Upload of first task solution

---
+++ Assignement1Test/CaseTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1;
+
+
+namespace Assignement1Test
+{
+    class CaseTest
+    {
+        static void Main(string[] Args)
+        {
+
+
+            Case1();
+            Case2();
+            Case3();
+            Console.Read();
+
+
+        }
+
+        static void Case1()
+        {
+            var topStudents = new List<Student>()
+            {
+                new Student (" Ivan ", jmbag :" 001234567 "),
+                new Student (" Luka ", jmbag :" 3274272 "),
+                new Student ("Ana", jmbag :" 9382832 ")
+            };
+            var ivan = new Student(" Ivan ", jmbag: " 001234567 ");
+            // false :(
+            bool isIvanTopStudent = topStudents.Contains(ivan);
+            Console.WriteLine(isIvanTopStudent.ToString());
+        }
+
+
+        static void Case2()
+        {
+            var list = new List<Student>()
+            {
+                new Student (" Ivan ", jmbag :" 001234567 "),
+                new Student (" Ivan ", jmbag :" 001234567 ")
+            };
+            // 2 :(
+            var distinctStudentsCount = list.Distinct().Count();
+            Console.WriteLine(distinctStudentsCount.ToString());
+        }
+
+
+        static void Case3()
+        {
+            var topStudents = new List<Student>()
+            {
+                new Student (" Ivan ", jmbag :" 001234567 "),
+                new Student (" Luka ", jmbag :" 3274272 "),
+                new Student ("Ana", jmbag :" 9382832 ")
+            };
+            var ivan = new Student(" Ivan ", jmbag: " 001234567 ");
+            // false :(
+            // == operator is a different operation from . Equals ()
+            // Maybe it isn ’t such a bad idea to override it as well
+            bool isIvanTopStudent = topStudents.Any(s => s == ivan);
+            Console.WriteLine(isIvanTopStudent.ToString());
+        }
+    }
+}
---
+++ Assignment1/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+
+
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            Student comparison = (Student) obj;
+            if (this.Jmbag == comparison.Jmbag)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator == (Student student1, Student student2)
+        {
+            return student1.Equals(student2);
+        }
+
+        public static bool operator != (Student student1, Student student2)
+        {
+            return !student1.Equals(student2);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Jmbag.GetHashCode();
+        }
+    }
+
+    public enum Gender
+    {
+        Male, Female
+    }
+}
